2122e6dff6478973430e60d2250220cd8e584f04
Masato Ohta <ohta.masato89@gmail.com>
2020-04-25 11:53:26 +0900

    Added swagger

--- src/app/main.go
+++ src/app/main.go
@@ -6,5 +6,9 @@ import (
 )
 
 func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
 	beego.Run()
 }
