e677bb3db3efc6b3ab0c3c4e096c03331cec7461
Masato Ohta <ohta.masato89@gmail.com>
2020-04-25 11:57:29 +0900

    Set MySQL as default database

--- src/app/main.go
+++ src/app/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	_ "app/routers"
+	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
@@ -11,4 +14,13 @@ func main() {
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
 	beego.Run()
+}
+
+func init() {
+	orm.RegisterDriver(beego.AppConfig.String("driver"), orm.DRMySQL)
+	orm.RegisterDataBase("default", beego.AppConfig.String("driver"), beego.AppConfig.String("sqlconn")+"?charset=utf8")
+	err := orm.RunSyncdb("default", false, false)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
